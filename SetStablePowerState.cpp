e2c06f42a333812b5cdde12a064ab1729828d80a
Moritz Heinemann <mo@blog-srv.net>
2022-04-12 13:42:10 +0200

    format

--- SetStablePowerState.cpp
+++ SetStablePowerState.cpp
@@ -1,26 +1,21 @@
+#include <cstdio>
 #include <dxgi1_4.h>
 #include <d3d12.h>
-#include <stdio.h>
 
-void Error(const char *str)
-{
+void Error(const char* str) {
     fprintf(stderr, "ERROR: %s\n", str);
     Sleep(INFINITE);
 }
 
-void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter)
-{
+void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter) {
     *ppAdapter = nullptr;
-    for (UINT AdapterIndex = 0; ; ++AdapterIndex)
-    {
+    for (UINT AdapterIndex = 0;; ++AdapterIndex) {
         IDXGIAdapter1* pAdapter = nullptr;
-        if (DXGI_ERROR_NOT_FOUND == pFactory->EnumAdapters1(AdapterIndex, &pAdapter))
-        {
+        if (DXGI_ERROR_NOT_FOUND == pFactory->EnumAdapters1(AdapterIndex, &pAdapter)) {
             break;
         }
 
-        if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
-        {
+        if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr))) {
             *ppAdapter = pAdapter;
             return;
         }
@@ -28,29 +23,24 @@ void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     IDXGIFactory4* pFactory = nullptr;
-    if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&pFactory))))
-    {
+    if (FAILED(CreateDXGIFactory1(IID_PPV_ARGS(&pFactory)))) {
         Error("CreateDXGIFactory1 failed");
     }
 
     IDXGIAdapter1* pAdapter = nullptr;
     GetHardwareAdapter(pFactory, &pAdapter);
-    if (!pAdapter)
-    {
+    if (!pAdapter) {
         Error("Failed to find DX12-compatible DXGI adapter");
     }
 
     ID3D12Device* pDevice = nullptr;
-    if (FAILED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pDevice))))
-    {
+    if (FAILED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&pDevice)))) {
         Error("D3D12CreateDevice failed for Adapter");
     }
 
-    if (FAILED(pDevice->SetStablePowerState(TRUE)))
-    {
+    if (FAILED(pDevice->SetStablePowerState(TRUE))) {
         Error("SetStablePowerState failed. Do you have the Win10 SDK installed?");
     }
 
